ac5d6d0e1f3ca638471a86e1da8f8adaec65a5d7
Morten Gram-Hansen <icyxgram@gmail.com>
2022-04-10 16:58:27 +0200

    Standings fixed 100%

--- DRS - Dynamisk Rangerings System/Pages/Standings/Standings.cshtml.cs
+++ DRS - Dynamisk Rangerings System/Pages/Standings/Standings.cshtml.cs
@@ -18,8 +18,6 @@ namespace DRS___Dynamisk_Rangerings_System.Pages.Standings
         public bool IsAdmin { get; set; }
         private ParticipantService ParticipantService { get; set; }
         private SettingsService SettingsService { get; set; }
-        private ParticipantService participantService;
-        private SettingsService settingsService;
         public List<Tuple<int,int, Models.Participant>> PointPairing { get; set; }
 
         [BindProperty] public int NumberOfParticipants { get; set; }
@@ -39,8 +37,6 @@ namespace DRS___Dynamisk_Rangerings_System.Pages.Standings
             ParticipantListItems = new List<SelectListItem>() { new SelectListItem("Vælg en deltager..", "0") };
             ParticipantListItems.AddRange(Participants.Select(p => new SelectListItem(p.Name, p.Id.ToString())).ToList());
 
-            this.settingsService = settingsService;
-            this.participantService = participantService;
             NumberOfParticipants = settingsService.GetMaxParticipants();
         }
 
@@ -71,85 +67,79 @@ namespace DRS___Dynamisk_Rangerings_System.Pages.Standings
 
         public IActionResult OnGet()
         {
-            Participants = participantService.GetParticipants().ToList();
-            PointPairing = participantService.PointPairing(Participants);
+            Participants = ParticipantService.GetParticipants().ToList();
+            PointPairing = ParticipantService.PointPairing(Participants);
             return Page();
         }
 
         #region OnGet Sorts
         public IActionResult OnGetSortByName()
-        {
-            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.Name).ToList();
-           Participants = participantService.SortParticipants(ParticipantService.SortingOptions.Name).ToList();
-           PointPairing = participantService.PointPairing(Participants);
+        { 
+           Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.Name).ToList();
+           PointPairing = ParticipantService.PointPairing(Participants);
             return Page();
         }
         public IActionResult OnGetSortByPoints()
         {
             Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.TotalPoints).ToList();
-            Participants = participantService.SortParticipants(ParticipantService.SortingOptions.TotalPoints).ToList();
-            PointPairing = participantService.PointPairing(Participants);
             return Page();
         }
         public IActionResult OnGetSortByPointsDescending()
         {
-            Participants = participantService.SortParticipants(ParticipantService.SortingOptions.TotalPoints).ToList();
-            PointPairing = participantService.PointPairing(Participants);
+            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.TotalPoints).ToList();
+            PointPairing = ParticipantService.PointPairing(Participants);
             PointPairing.Reverse();
             return Page();
         }
         public IActionResult OnGetSortByWins()
         {
            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.TotalWin).ToList();
-           Participants = participantService.SortParticipants(ParticipantService.SortingOptions.TotalWin).ToList();
-           PointPairing = participantService.PointPairing(Participants);
+           PointPairing = ParticipantService.PointPairing(Participants);
            return Page();
         }
         public IActionResult OnGetSortByWinsDescending()
         {
-            Participants = participantService.SortParticipants(ParticipantService.SortingOptions.TotalWin).ToList();
-            PointPairing = participantService.PointPairing(Participants);
+            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.TotalWin).ToList();
+            PointPairing = ParticipantService.PointPairing(Participants);
             PointPairing.Reverse();
             return Page();
         }
         public IActionResult OnGetSortByMatches()
         {
-            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.TotalMatches).ToList(); 
-            Participants = participantService.SortParticipants(ParticipantService.SortingOptions.TotalMatches).ToList();
-            PointPairing = participantService.PointPairing(Participants);
+            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.TotalMatches).ToList();
+            PointPairing = ParticipantService.PointPairing(Participants);
             return Page();
         }
         public IActionResult OnGetSortByMatchesDescending()
         {
-            Participants = participantService.SortParticipants(ParticipantService.SortingOptions.TotalMatches).ToList();
-            PointPairing = participantService.PointPairing(Participants);
+            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.TotalMatches).ToList();
+            PointPairing = ParticipantService.PointPairing(Participants);
             PointPairing.Reverse();
             return Page();
         }
         public IActionResult OnGetSortBySeconds()
         {
-            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.Total2nd).ToList(); 
-            Participants = participantService.SortParticipants(ParticipantService.SortingOptions.Total2nd).ToList();
-            PointPairing = participantService.PointPairing(Participants);
+            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.Total2nd).ToList();
+            PointPairing = ParticipantService.PointPairing(Participants);
             return Page();
         }
         public IActionResult OnGetSortBySecondsDescending()
         {
-            Participants = participantService.SortParticipants(ParticipantService.SortingOptions.Total2nd).ToList();
-            PointPairing = participantService.PointPairing(Participants);
+            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.Total2nd).ToList();
+            PointPairing = ParticipantService.PointPairing(Participants);
             PointPairing.Reverse();
             return Page();
         }
         public IActionResult OnGetSortByLoss()
         {
-            Participants = participantService.SortParticipants(ParticipantService.SortingOptions.TotalLoss).ToList();
-            PointPairing = participantService.PointPairing(Participants);
+            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.TotalLoss).ToList();
+            PointPairing = ParticipantService.PointPairing(Participants);
             return Page();
         }
         public IActionResult OnGetSortByLossDescending()
         {
-            Participants = participantService.SortParticipants(ParticipantService.SortingOptions.TotalLoss).ToList();
-            PointPairing = participantService.PointPairing(Participants);
+            Participants = ParticipantService.SortParticipants(ParticipantService.SortingOptions.TotalLoss).ToList();
+            PointPairing = ParticipantService.PointPairing(Participants);
             PointPairing.Reverse();
             return Page();
         }
